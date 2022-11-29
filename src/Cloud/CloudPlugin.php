4cdc14456956ae0220ad8a0fbdcec144ad0c18cd
Serenity-Sun <DreamTravel1227@gmail.com>
2022-11-30 00:43:17 +0600

    Initial commit

---
+++ src/Cloud/CloudPlugin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Cloud;
+
+use pocketmine\plugin\PluginBase;
+
+class CloudPlugin extends PluginBase {
+
+	/**
+	 * @return void
+	 */
+    protected function onEnable(): void
+    {
+
+    }
+}
