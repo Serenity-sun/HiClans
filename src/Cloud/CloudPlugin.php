2231d5256254597520146d989a8ced55716ed21f
Serenity-Sun <DreamTravel1227@gmail.com>
2022-12-03 21:25:11 +0600

    Test virion form

--- src/Cloud/CloudPlugin.php
+++ src/Cloud/CloudPlugin.php
@@ -2,9 +2,11 @@
 
 namespace Cloud;
 
+use dktapps\pmforms\ModalForm;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
-use Vecnavium\FormsUI\CustomForm;
 
 class CloudPlugin extends PluginBase {
 
@@ -14,4 +16,27 @@ class CloudPlugin extends PluginBase {
     protected function onEnable(): void
     {
     }
+
+	/**
+	 * @param CommandSender $sender
+	 * @param Command $command
+	 * @param string $label
+	 * @param array $args
+	 * @return bool
+	 */
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+	{
+		$form = new ModalForm(
+			"example title",
+			"example content",
+			function (Player $player, bool $choice) {
+				$player->sendMessage("oke");
+			});
+
+		if ($sender instanceof Player) {
+			$sender->sendForm($form);
+		}
+
+		return false;
+	}
 }
